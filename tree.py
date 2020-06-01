7ed5f17328c67ac62db96529b137cff3736389e7
Kayam, Harshith <hkayam3@gatech.edu>
2020-06-01 12:38:45 -0300

    Update tree.py
    testing segment tree

--- tree.py
+++ tree.py
@@ -18,7 +18,10 @@ def seg_tree(arr):
     segment_tree = [None] * len(arr)
     segment_tree.extend(arr)
     for i in range(len(arr) - 1, 1):
+        print('HELLO')
+        print(min((segment_tree[2*i], segment_tree[2*i + 1])))
         segment_tree[i] = min((segment_tree[2*i], segment_tree[2*i + 1]))
+    return segment_tree
 
 def rmq(left, right, arr):
     seg_tree = seg_tree(arr)
@@ -36,12 +39,14 @@ def rmq(left, right, arr):
         right /= 2
     return minimum
 
-def lca(Tree, a, b):
-    node_list = []
-    index_list = []
-    first_occ = []
-
-    nl, il, fo, _ = euler_tour(self.root, node_list, index_list, first_occ, 0
+# def lca(Tree, a, b):
+#     node_list = []
+#     index_list = []
+#     first_occ = []
+#
+#     nl, il, fo, _ = euler_tour(self.root, node_list, index_list, first_occ, 0
+#
+#     return 0
 
 class Node(object):
     def __init__(self, name):
@@ -65,7 +70,7 @@ class Tree(object):
     def __init__(self, root):
         self.root = root
 
-        nl, il, fo, _ = euler_tour(tree.root, [], [], [], 0)
+        nl, il, fo, _ = euler_tour(self.root, [], [], [], 0)
         self.node_list = nl
         self.index_list = il
         self.first_occ = fo
@@ -95,6 +100,11 @@ if __name__== "__main__":
 
      tree = Tree(root)
 
+     print("EULER TOUR TEST")
      print(tree.node_list)
-     print(index_list)
-     print(first_occ)
+     print(tree.index_list)
+     print(tree.first_occ)
+
+     print("SEG TREE TEST")
+     arr = [1,5,3,7,3,6,5,7]
+     print(seg_tree(arr))
